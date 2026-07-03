7b08c1f086d0227e071a722df7f301f051a34a32
StanleyGodbehere <stanleygodbehere@gmail.com>
2026-07-03 02:25:30 +0100

    training/testing code skeleton

--- train.py
+++ train.py
@@ -3,12 +3,15 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-from torch.utils.data import Dataset
+import torch.nn as nn
+import torch.optim as optimisers
+from torch.utils.data import Dataset, DataLoader, random_split
+from model import UNet
 
 DATA_PATH = "./BraTS2020_TrainingData/BraTS2020_training_data/content/data"
-
 EPOCHS = 25
 LEARNING_RATE = 0.001
+BATCH_SIZE = 10
 
 class Data(Dataset):
     def __init__(self, dataPath):
@@ -22,42 +25,47 @@ class Data(Dataset):
             image = file["image"][:].astype(np.float32)
             mask = file["mask"][:].astype(np.float32)
         
-        inputTensor = torch.from_numpy(image)
-        target = torch.from_numpy(mask)
+        inputTensor = torch.from_numpy(image.transpose(2, 0, 1))
+        target = torch.from_numpy(mask.transpose(2, 0, 1))
 
         return inputTensor, target
+    
+def costFunction():
+    return
+    
+def trainEpoch(model, optimiser, dataLoader):
+    model.train()
+    totalCost = 0
+    for inputTensor, target in dataLoader:
+        optimiser.zero_grad()
+        cost = costFunction(model(inputTensor), target)
+        cost.backward()
+        optimiser.step()
+        totalCost += cost.item() * inputTensor.size(0)
+    return totalCost / len(dataLoader.dataset)
 
+@torch.no_grad()
+def test(model, dataLoader):
+    model.eval()
+    totalCost = 0
+    for inputTensor, target in dataLoader:
+        cost = costFunction(model(inputTensor), target)
+        totalCost += cost.item() * inputTensor.size(0)
+    return totalCost / len(dataLoader.dataset)
 
-if 1 == 1:
-    quit()
+if __name__ == "__main__":
+    data = Data(DATA_PATH)
+    trainingSplit = int(0.8 * len(data))
+    trainingData, testingData = random_split(data, [trainingSplit, len(data) - trainingSplit], generator=torch.Generator().manual_seed(19))
 
-#slices = glob.glob("./BraTS2020_TrainingData/BraTS2020_training_data/content/data/*.h5")
-slices = glob.glob("./test/*.h5")
+    trainingDataLoader = DataLoader(trainingData, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+    testingDataLoader = DataLoader(trainingData, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 
-for slicePath in sorted(slices):
-    with h5py.File(slicePath, "r") as file:
-        image = file["image"][:]
-        mask = file["mask"][:]
+    model = UNet([4,32,64,128,3])
+    optimiser = optimisers.SGD(model.parameters(), lr=LEARNING_RATE)
 
-    fig, axes = plt.subplots(1, 5, figsize=(14, 7))
-
-    fig.suptitle(slicePath.split("\\")[-1])
-
-    colours = np.array([[1,0,0],[0,1,0],[0,0.5,1]], dtype=np.float32)
-
-    for y in range(4):
-        img = image[..., y].astype(np.float32)
-
-        axes[y].imshow(img)
-
-    rgb = np.zeros((240, 240, 3), dtype=np.float32)
-    colours = np.array([[1,0,0],[0,1,0],[0,0,1]])
-    for i in range(3):
-        channelMask = mask[..., i].astype(bool)
-        rgb[channelMask] = colours[i]
-
-    axes[4].imshow(rgb)
-
-    plt.tight_layout()
-    plt.pause(1)
-    plt.close()
+    for i in range(EPOCHS):
+        trainingCost = trainEpoch(model, optimiser, trainingDataLoader)
+        testingCost = test(model, testingDataLoader)
+        print("Epoch",i,":\ntraining cost =",trainingCost,"\ntesting cost =",testingCost)
+        
